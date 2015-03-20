c4632b577fc8e2b0554e0513d9fd68b8cc13bd54
Andreas <Andreas@AS_Hjemme_PC>
2015-03-20 17:46:41 +0100

    1.commit

---
+++ Java_F_C_B/src/userinput/Inputboxes.java
@@ -0,0 +1,18 @@
+package userinput;
+
+import javax.swing.JEditorPane;
+import javax.swing.JOptionPane;
+public class Inputboxes {
+	public static void main(String[] args) {
+		
+		String first_name, family_name, full_name;
+		
+		first_name = JOptionPane.showInputDialog("First Name", "Enter your First Name");
+		family_name = JOptionPane.showInputDialog("Family Name", "Enter your Family Name");
+		full_name = "Welcome " + first_name + " " + family_name;
+		
+		JOptionPane.showMessageDialog(null,full_name, "Name", JOptionPane.INFORMATION_MESSAGE);
+		System.exit(0);
+	}
+
+}
